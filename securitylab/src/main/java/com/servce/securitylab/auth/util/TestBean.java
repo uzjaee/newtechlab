77421829ab3c634a65304e78102a45b689a24871
uzjaee <kyj960802@gmail.com>
2024-09-11 00:17:51 +0900

    feat/쿠키 생성 및 테스트

--- securitylab/src/main/java/com/servce/securitylab/auth/util/TestBean.java
+++ securitylab/src/main/java/com/servce/securitylab/auth/util/TestBean.java
@@ -1,4 +1,4 @@
-package com.servce.securitylab.auth;
+package com.servce.securitylab.auth.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
