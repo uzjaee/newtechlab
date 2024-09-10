06b712b3a43e8e29ae96ccd92655c94d1931a76e
uzjaee <kyj960802@gmail.com>
2024-09-10 17:20:17 +0900

    feat: 외부 api 사용과제 태그와 카테고리 , 상태 request 값 추가 태그와 카테고리 ,상태 response 값 추가 service 매개변수와  service 사용시 인자 수정

--- outapi/src/main/java/sparta/outapi/CreatePetRequest.java
+++ outapi/src/main/java/sparta/outapi/CreatePetRequest.java
@@ -1,14 +1,35 @@
 package sparta.outapi;
 
 
+
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class CreatePetRequest {
   private Long petId;
   private String name;
+  private Category category;
+  private List<String> photoUrls;
+  private List<Tag> tags;
+  private String status;
 
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Category{
+    private String name;
+  }
+
+  @Getter
+
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Tag{
+    private String name;
+  }
 }
--- outapi/src/main/java/sparta/outapi/OutapiApplication.java
+++ outapi/src/main/java/sparta/outapi/OutapiApplication.java
@@ -1,8 +1,14 @@
 package sparta.outapi;
 
 import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sparta.outapi.CreatePetRequest.Category;
+import sparta.outapi.CreatePetRequest.Tag;
 
 @SpringBootApplication
 public class OutapiApplication {
@@ -20,9 +26,32 @@ public class OutapiApplication {
 
 	@PostConstruct
 	public void init() {
-		PetCreatResponse createResponse =  swaggerService.createPetData(1L,"sparta");
+		Category category = new Category("cat");
+		List<String> photos = Arrays.asList("photo1,ptoto2");
+		List<Tag> tags = Arrays.asList(new Tag("cat"));
+		PetCreatResponse createResponse =  swaggerService.createPetData(
+				1L,
+				"sparta",
+				new Category("dog"),
+				photos,
+				tags,
+				"available");
 		PetResponse response = swaggerService.getPetData(createResponse.getId());
-		System.out.println("petId = " + response.getId() + ", name = " + response.getName() + ", images= " + response.getPhotos() + ", tags = " + response.getTags());
-	}
+		System.out.println("""
+				petId : %d
+				Name : %s
+				images : %s
+				tags : %s
+				category : %s
+				status : %s
+				""".formatted(
+				response.getId(),
+				response.getName(),
+				response.getPhotos().stream().collect(Collectors.joining(",")),
+				response.getTags().stream().map(Tag::getName).collect(Collectors.joining(",")),
+				response.getCategory().getName(),
+				response.getStatus()
+		));
+		}
 
 }
