341a29fbf0e9d5638f80591b4c0be8dfb6eaae99
uzjaee <kyj960802@gmail.com>
2024-09-11 13:35:18 +0900

    feat : 회원가입 service 구현
    - 이메일 ,닉네임 항목 중복 검사
    - 회원 비밀번호 암호화
    - 비밀번호 암호화를 위한 security 의존성 추가
    - passwordEncoder 사용을 위한 Bean 등록 configuration 추가

--- securitylab/src/main/java/com/servce/securitylab/SecuritylabApplication.java
+++ securitylab/src/main/java/com/servce/securitylab/SecuritylabApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SecuritylabApplication {
 
   public static void main(String[] args) {
