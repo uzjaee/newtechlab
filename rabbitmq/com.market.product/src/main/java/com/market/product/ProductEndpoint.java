ed485a9201adcc5cabb1b94a4ee359980057d3ae
uzjaee <kyj960802@gmail.com>
2024-11-12 03:49:00 +0900

    feat: rabbitmq payment consumer 기능 추가

---
+++ rabbitmq/com.market.product/src/main/java/com/market/product/ProductEndpoint.java
@@ -0,0 +1,19 @@
+package com.market.product;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ProductEndpoint {
+  @Value("${spring.application.name}")
+  private String applicationName;
+
+  @RabbitListener(queues = "${message.queue.product}")
+  public void receiveMessage(String orderId) {
+    log.info("Received orderId: {} , appname: {}", orderId, applicationName);
+  }
+
+}
